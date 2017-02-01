0a4b9088df18559b9c577fc1335363e7c57e30b0
Hobin Yoon <hobinyoon@gmail.com>
2017-02-01 11:18:44 -0500

    update

--- s3-bucket-mgmt.sh
+++ s3-bucket-mgmt.sh
@@ -17,3 +17,15 @@ done
 
 # I have less than 2.6GB stored now, which is about $0.06 / Month with $0.023 /
 # GB / Month. That's okay.
+
+# AMI backed by EBS gp3
+#   $0.10/GB/Month
+#   I have 11 AMIs. Let's say the average is about 10GB.
+#   11 * 10GB * $0.1/GB/Month = $11 Month. This is something to think about.
+#
+# Move old AMIs to the machines at gatech. Like to glacier.
+
+# Data transfer out to Internet: $0.09 per GB
+#   Compared with the storage price, it's better to move it out when you don't use it for more than 1 month.
+
+# Cleaning up a lot of not-so-important AMIs
