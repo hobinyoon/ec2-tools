b3298be633adf439702a81452f38a2ffca74f39a
Hobin Yoon <hobinyoon@gmail.com>
2017-02-02 02:54:41 -0500

    update

--- lib/ReqSpotInsts.py
+++ lib/ReqSpotInsts.py
@@ -86,12 +86,15 @@ class _Req:
 
 		# This is run as root. Some of them need to be run by the user ubuntu
 		#   http://unix.stackexchange.com/questions/4342/how-do-i-get-sudo-u-user-to-use-the-users-env
+		#
+		# running command through ssh is to have ulimit in effect, which works only in a login shell.
 		user_data = \
 """#!/bin/bash
 sudo rm -rf /home/ubuntu/work/mutant/ec2-tools
 sudo -i -u ubuntu bash -c 'git clone https://github.com/hobinyoon/mutant-ec2-tools.git /home/ubuntu/work/mutant/ec2-tools'
-sudo -i -u ubuntu /home/ubuntu/work/mutant/ec2-tools/lib/ec2-init.py {0}
+ssh ubuntu@localhost /home/ubuntu/work/mutant/ec2-tools/lib/ec2-init.py {0}
 """
+		#sudo -i -u ubuntu /home/ubuntu/work/mutant/ec2-tools/lib/ec2-init.py {0}
 		user_data = user_data.format(base64.b64encode(zlib.compress(json.dumps(self.params))))
 
 		block_dev_mappings = []
