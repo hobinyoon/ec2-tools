7668fc237eb89a96f45c6adfe651d00ec3fd6637
Hobin Yoon <hobinyoon@gmail.com>
2017-02-08 17:20:48 -0500

    update

--- launch-jobs.py
+++ launch-jobs.py
@@ -42,8 +42,90 @@ def main(argv):
 	globals()[job]()
 
 
-# TODO: separate into different files so that you can easily compare and make
-# sure you don't make any mistakes.
+def Job_UnmodifiedRocksDbWithWithoutMetadataCachingByStgDevs():
+	class Conf:
+		exp_per_ec2inst = 5
+		def __init__(self, stg_dev):
+			self.stg_dev = stg_dev
+			self.metadata_caching = []
+		def Full(self):
+			return (len(self.metadata_caching) >= Conf.exp_per_ec2inst)
+		def Add(self, metadata_caching):
+			self.metadata_caching.append(metadata_caching)
+		def Size(self):
+			return len(self.metadata_caching)
+		def __repr__(self):
+			return "(%s, %s)" % (self.stg_dev, self.metadata_caching)
+
+	# The lower bound without getting the system overloaded are different for
+	# different storage devices.  local-ssd1 and ebs-gp2 have 14, which is 1.4GB,
+	# which the range() function sets the lower bound as 12. They are capped by
+	# the main memory. OOM killer.  ebs-st1 can go 1.6GB without the storage
+	# device overloaded, ebs-sc1 2.0GB.
+	num_exp_per_conf = 4
+	confs = []
+	for stg_dev in ["local-ssd1", "ebs-gp2", "ebs-st1", "ebs-sc1"]:
+		conf = Conf(stg_dev)
+		for j in range(num_exp_per_conf):
+			for i in [True, False]:
+				if conf.Full():
+					confs.append(conf)
+					conf = Conf(stg_dev)
+				conf.Add(i)
+		if conf.Size() > 0:
+			confs.append(conf)
+
+	Cons.P("%d machines" % len(confs))
+	Cons.P(pprint.pformat(confs, width=100))
+	sys.exit(0)
+
+	for conf in confs:
+		params = { \
+				# us-east-1, which is where the S3 buckets for experiment are.
+				"region": "us-east-1"
+				, "inst_type": "c3.2xlarge"
+				, "spot_req_max_price": 1.0
+				# RocksDB can use the same AMI
+				, "init_script": "mutant-cassandra-server-dev"
+				, "ami_name": "mutant-cassandra-server"
+				, "block_storage_devs": []
+				, "unzip_quizup_data": "true"
+				, "run_cassandra_server": "false"
+				# For now, it doesn't do much other than checking out the code and building.
+				, "rocksdb": { }
+				, "rocksdb-quizup-runs": []
+				, "terminate_inst_when_done": "true"
+				}
+		if conf.stg_dev == "local-ssd1":
+			pass
+		elif conf.stg_dev == "ebs-gp2":
+			params["block_storage_devs"].append({"VolumeType": "gp2", "VolumeSize": 1000, "DeviceName": "d"})
+		elif conf.stg_dev == "ebs-st1":
+			params["block_storage_devs"].append({"VolumeType": "st1", "VolumeSize": 3000, "DeviceName": "e"})
+		elif conf.stg_dev == "ebs-sc1":
+			params["block_storage_devs"].append({"VolumeType": "sc1", "VolumeSize": 3000, "DeviceName": "f"})
+		else:
+			raise RuntimeError("Unexpected")
+
+		p1 = { \
+				"exp_desc": "Unmodified RocksDB latency by different memory sizes"
+				, "fast_dev_path": "/mnt/%s/rocksdb-data" % conf.stg_dev
+				, "db_path": "/mnt/%s/rocksdb-data/quizup" % conf.stg_dev
+				, "init_db_to_90p_loaded": "true"
+				, "evict_cached_data": "true"
+				, "memory_limit_in_mb": 1024 * 3
+
+				, "mutant_enabled": "false"
+				, "workload_start_from": 0.899
+				, "workload_stop_at":    -1.0
+				, "simulation_time_dur_in_sec": 60000
+				}
+		for ms in conf.mem_sizes:
+			p1["memory_limit_in_mb"] = 1024.0 * ms
+			params["rocksdb-quizup-runs"].append(dict(p1))
+		LaunchJob(params)
+
+
 def Job_2LevelMutantLatencyByColdStgBySstMigTempThresholds():
 	class Conf:
 		exp_per_ec2inst = 7
