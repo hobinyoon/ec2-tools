d4e6789829bfc319602c86543be5d0a036872729
Hobin Yoon <hobinyoon@gmail.com>
2017-02-13 12:43:20 -0500

    update

--- launch-jobs.py
+++ launch-jobs.py
@@ -2,6 +2,7 @@
 
 import boto3
 import botocore
+import inspect
 import json
 import math
 import os
@@ -42,6 +43,115 @@ def main(argv):
 	globals()[job]()
 
 
+def Job_AggressiveCompactions():
+	params = { \
+			"region": "us-east-1"
+			, "inst_type": "c3.2xlarge"
+			, "spot_req_max_price": 1.0
+			, "init_script": "mutant-cassandra-server-dev"
+			, "ami_name": "mutant-cassandra-server"
+			, "block_storage_devs": []
+			, "unzip_quizup_data": "true"
+			, "run_cassandra_server": "false"
+			# For now, it doesn't do much other than checking out the code and building.
+			, "rocksdb": { }
+			, "rocksdb-quizup-runs": []
+			, "terminate_inst_when_done": "true"
+			}
+
+	# Full time range experiments are for the number of compactions and SSTables.
+	# They don't show the number of disk IOs.
+	if False:
+		# From 2^-10 = 0.0009765625 down to 2^-32
+		for i in range(-10, -34, -2):
+			sst_mig_temp_thrds = pow(2, i)
+			p1 = { \
+					# Use the current function name since you always forget to set this
+					"exp_desc": inspect.currentframe().f_code.co_name[4:]
+					, "fast_dev_path": "/mnt/local-ssd1/rocksdb-data"
+					, "slow_dev_paths": {}
+					, "db_path": "/mnt/local-ssd1/rocksdb-data/quizup"
+					, "init_db_to_90p_loaded": "false"
+					, "evict_cached_data": "true"
+					, "memory_limit_in_mb": 9.0 * 1024
+
+					# This doesn't really matter for measuring the number of compactions
+					# and SSTables.
+					, "cache_filter_index_at_all_levels": "false"
+					, "monitor_temp": "true"
+					, "migrate_sstables": "true"
+					, "workload_start_from": -1.0
+					, "workload_stop_at":    -1.0
+					, "simulation_time_dur_in_sec": 20000
+					, "sst_migration_temperature_threshold": sst_mig_temp_thrds
+					}
+			params["rocksdb-quizup-runs"] = []
+			params["rocksdb-quizup-runs"].append(dict(p1))
+			#Cons.P(pprint.pformat(params))
+			LaunchJob(params)
+
+	# 95% to 100% time range experiments for measuring latency and the number of IOs.
+	if True:
+		class Conf:
+			exp_per_ec2inst = 2
+			def __init__(self):
+				self.sst_mig_temp_thrds = []
+			def Full(self):
+				return (len(self.sst_mig_temp_thrds) >= Conf.exp_per_ec2inst)
+			def Add(self, smtt):
+				self.sst_mig_temp_thrds.append(smtt)
+			def Size(self):
+				return len(self.sst_mig_temp_thrds)
+			def __repr__(self):
+				return "(%s)" % (self.sst_mig_temp_thrds)
+
+		num_exp_per_conf = 2
+		confs = []
+		conf = Conf()
+		for j in range(num_exp_per_conf):
+			# From 2^-10 = 0.0009765625 down to 2^-32
+			for i in range(-10, -34, -2):
+				if conf.Full():
+					confs.append(conf)
+					conf = Conf()
+				mig_temp_thrds = math.pow(2, i)
+				conf.Add(mig_temp_thrds)
+		if conf.Size() > 0:
+			confs.append(conf)
+
+		Cons.P("%d machines" % len(confs))
+		Cons.P(pprint.pformat(confs, width=100))
+		#sys.exit(1)
+
+		for conf in confs:
+			params["rocksdb-quizup-runs"] = []
+			for mt in conf.sst_mig_temp_thrds:
+				p1 = { \
+						# Use the current function name since you always forget to set this
+						"exp_desc": inspect.currentframe().f_code.co_name[4:]
+						, "fast_dev_path": "/mnt/local-ssd1/rocksdb-data"
+						, "slow_dev_paths": {}
+						, "db_path": "/mnt/local-ssd1/rocksdb-data/quizup"
+						, "init_db_to_90p_loaded": "true"
+						, "evict_cached_data": "true"
+						, "memory_limit_in_mb": 2.0 * 1024
+
+						# Not caching metadata might be a better idea. So the story is you
+						# present each of the optimizations separately, followed by the
+						# combined result.
+						, "cache_filter_index_at_all_levels": "false"
+						, "monitor_temp": "true"
+						, "migrate_sstables": "true"
+						, "workload_start_from": 0.899
+						, "workload_stop_at":    -1.0
+						, "simulation_time_dur_in_sec": 60000
+						, "sst_migration_temperature_threshold": mt
+						}
+				params["rocksdb-quizup-runs"].append(dict(p1))
+			#Cons.P(pprint.pformat(params))
+			LaunchJob(params)
+
+
 def Job_2LevelMutantBySstMigTempThresholdsToMeasureStorageUsage():
 	class Conf:
 		exp_per_ec2inst = 2
@@ -68,7 +178,8 @@ def Job_2LevelMutantBySstMigTempThresholdsToMeasureStorageUsage():
 		for slow_dev in ["ebs-gp2"]:
 			conf = Conf(slow_dev)
 			for j in range(num_exp_per_conf):
-				for i in range(-14, 12, 2):
+				#for i in range(-14, 12, 2):
+				for i in range(-20, -14, 2):
 					if conf.Full():
 						confs.append(conf)
 						conf = Conf(slow_dev)
@@ -79,7 +190,6 @@ def Job_2LevelMutantBySstMigTempThresholdsToMeasureStorageUsage():
 
 	Cons.P("%d machines" % len(confs))
 	Cons.P(pprint.pformat(confs, width=100))
-	#sys.exit(0)
 
 	for conf in confs:
 		params = { \
@@ -126,7 +236,47 @@ def Job_2LevelMutantBySstMigTempThresholdsToMeasureStorageUsage():
 		#Cons.P(pprint.pformat(params))
 		LaunchJob(params)
 
-	# Run unmodified RocksDB too for a comparison
+	# RocksDB with temperature monitoring and no sstable migration for a comparision
+	params = { \
+			# us-east-1, which is where the S3 buckets for experiment are.
+			"region": "us-east-1"
+			, "inst_type": "c3.2xlarge"
+			, "spot_req_max_price": 1.0
+			# RocksDB can use the same AMI
+			, "init_script": "mutant-cassandra-server-dev"
+			, "ami_name": "mutant-cassandra-server"
+			, "block_storage_devs": []
+			, "unzip_quizup_data": "true"
+			, "run_cassandra_server": "false"
+			# For now, it doesn't do much other than checking out the code and building.
+			, "rocksdb": { }
+			, "rocksdb-quizup-runs": []
+			, "terminate_inst_when_done": "true"
+			}
+	p1 = { \
+			"exp_desc": "Mutant latency by cold storge devices by SSTable migration temperature thresholds"
+			, "fast_dev_path": "/mnt/local-ssd1/rocksdb-data"
+			, "db_path": "/mnt/local-ssd1/rocksdb-data/quizup"
+			, "init_db_to_90p_loaded": "false"
+			, "evict_cached_data": "true"
+			, "memory_limit_in_mb": 9.0 * 1024
+
+			# Doesn't matter. We are interested in the size only.
+			, "cache_filter_index_at_all_levels": "false"
+
+			, "monitor_temp": "true"
+			, "migrate_sstables": "false"
+			, "workload_start_from": -1.0
+			, "workload_stop_at":    -1.0
+			, "simulation_time_dur_in_sec": 20000
+			, "sst_migration_temperature_threshold": 0
+			}
+	params["rocksdb-quizup-runs"].append(dict(p1))
+	#Cons.P(pprint.pformat(params))
+	LaunchJob(params)
+	return
+
+	# Unmodified RocksDB too for a comparison
 	params = { \
 			# us-east-1, which is where the S3 buckets for experiment are.
 			"region": "us-east-1"
@@ -260,8 +410,8 @@ def Job_2LevelMutantLatencyByColdStgBySstMigTempThresholds():
 			self.sst_mig_temp_thrds = []
 		def Full(self):
 			return (len(self.sst_mig_temp_thrds) >= Conf.exp_per_ec2inst)
-		def Add(self, mem_size):
-			self.sst_mig_temp_thrds.append(mem_size)
+		def Add(self, smtt):
+			self.sst_mig_temp_thrds.append(smtt)
 		def Size(self):
 			return len(self.sst_mig_temp_thrds)
 		def __repr__(self):
